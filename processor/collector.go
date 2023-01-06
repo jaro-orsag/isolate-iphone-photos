d08bc17fa320e2f7e7e5efdb7533d007bde580cd
Jaroslav Orsag <jorsag@gohealth.com>
2023-01-06 22:25:17 +0100

    Implement processing of video and live photo metadata

--- processor/collector.go
+++ processor/collector.go
@@ -1,8 +1,12 @@
 package processor
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 
 	filepath "path/filepath"
 
@@ -10,66 +14,147 @@ import (
 	"github.com/rwcarlsen/goexif/mknote"
 )
 
+// TODO: this should be generalized or configurable
+const videoFileSuffix = ".mov"
+
 func MakeCollectMetadata(postProcess PostProcessFunc) filepath.WalkFunc {
 	exif.RegisterParsers(mknote.All...)
 
 	return func(path string, fileInfo os.FileInfo, err error) error {
 
-		log.Printf("processing %s\n", path)
+		modTime := fileInfo.ModTime()
 
 		if err != nil {
-			log.Printf("\terror processing path: %s\n", err)
 
-			return nil
+			return postProcess(createUnprocessable(path, modTime, err))
 		}
 
 		if fileInfo.IsDir() {
-			log.Printf("\tnot processing path to directory\n")
+			// not processing path to directory
 
 			return nil
 		}
 
 		imgFile, err := os.Open(path)
 		if err != nil {
-			log.Printf("\terror opening file: %s\n", err)
 
-			return nil
+			return postProcess(createUnprocessable(path, modTime, err))
+		}
+
+		if isVideo(path) {
+			// videos do not have exif metadata, so we are not even trying to load them
+
+			if isLivePhoto(path) {
+
+				return postProcess(createThrash(path, modTime, errors.New("live photo")))
+			}
+
+			return postProcess(createRegular(path, modTime))
 		}
 
 		exifData, err := exif.Decode(imgFile)
 		if err != nil {
-			log.Printf("\terror parsing file: %s\n", err)
+			// files without exif metadata are thrash, because iPhone camera photos have exif metadata
 
-			return nil
+			return postProcess(createThrash(path, modTime, err))
 		}
 
-		metadata := &metadata{
-			Path:     path,
-			Make:     getField(exifData, exif.Make),
-			Model:    getField(exifData, exif.Model),
-			DateTime: getField(exifData, exif.DateTime),
-		}
-
-		log.Printf("\tsuccess parsing file: %#v\n", metadata)
-
-		return postProcess(metadata)
+		return postProcess(createRegularWithExif(path, exifData, modTime))
 	}
 }
 
-func getField(metaData *exif.Exif, fieldName exif.FieldName) string {
+func createThrash(path string, dateCreated time.Time, err error) *metadata {
+
+	return &metadata{
+		Status:       Thrash,
+		ThrashReason: fmt.Sprint(err),
+		Path:         path,
+		Created:      &dateCreated,
+	}
+}
+
+func createUnprocessable(path string, dateCreated time.Time, err error) *metadata {
+
+	return &metadata{
+		Status:              Unprocessable,
+		UnprocessableReason: fmt.Sprint(err),
+		Path:                path,
+		Created:             &dateCreated,
+	}
+}
+
+func createRegular(path string, dateCreated time.Time) *metadata {
+
+	return &metadata{
+		Status:  Regular,
+		Path:    path,
+		Created: &dateCreated,
+	}
+}
+
+func createRegularWithExif(path string, exifData *exif.Exif, dateCreatedFallback time.Time) *metadata {
+
+	return &metadata{
+		Status:  Regular,
+		Path:    path,
+		Make:    getExifField(exifData, exif.Make),
+		Model:   getExifField(exifData, exif.Model),
+		Created: getExifTimeField(exifData, exif.DateTime, dateCreatedFallback),
+	}
+}
+
+func getExifField(metaData *exif.Exif, fieldName exif.FieldName) string {
 	fieldValue, err := metaData.Get(fieldName)
 	if err != nil {
 		log.Printf("\terror parsing field %s: %s", fieldName, err)
 
-		return "N/A"
+		return ""
 	}
 
 	fieldStringValue, err := fieldValue.StringVal()
 	if err != nil {
 		log.Printf("\terror converting value of %s to string: %s", fieldName, err)
 
-		return "N/A"
+		return ""
 	}
 
 	return fieldStringValue
+}
+
+func getExifTimeField(metaData *exif.Exif, fieldName exif.FieldName, fallbackTime time.Time) *time.Time {
+	fieldStr := getExifField(metaData, fieldName)
+
+	fieldDate, err := time.ParseInLocation("2006:01:02 15:04:05", fieldStr, time.Local)
+
+	if err != nil {
+		log.Printf("\terror converting value of %s to string: %s. Going to use defaultTime %s.", fieldName, err, fallbackTime)
+
+		return &fallbackTime
+	}
+
+	return &fieldDate
+}
+
+func isVideo(path string) bool {
+	return strings.HasSuffix(strings.ToLower(path), videoFileSuffix)
+}
+
+func isLivePhoto(path string) bool {
+	filesWithSameNameAndVariousExtensions := path[:len(path)-len(videoFileSuffix)] + "*"
+	matches, err := filepath.Glob(filesWithSameNameAndVariousExtensions)
+
+	if err != nil {
+		log.Printf("error evaluating if video is live photo, treating it as regular video for safety reasons: %s\n", path)
+		log.Println(err)
+
+		return false
+	}
+
+	if len(matches) > 1 {
+		// video is live photo, because regular photo with the same name (not extension) exists besides the video
+
+		return true
+	}
+
+	return false
 }
